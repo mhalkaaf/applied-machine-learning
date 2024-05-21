0d97cfd1efe070eb355008e82aaa5e645163c532
haeckal <haeckal@asaren.ai>
2024-05-21 20:35:58 +0700

    deploy

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,5 +1,6 @@
 const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
+const storeData = require('../services/storeData');
 
 async function postPredictHandler(request, h) {
     const { image } = request.payload;
@@ -18,6 +19,8 @@ async function postPredictHandler(request, h) {
         "confidenceScore": confidenceScore,
         "createdAt": createdAt
       }
+    
+    await storeData(id, data);
     
     const response = h.response({
         status: 'success',
